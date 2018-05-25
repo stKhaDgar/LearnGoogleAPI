90d2c9049cf42a2db25af2b20ab359f8b6ae5ea1
Andrey Berezhnoi <stmaktavish@gmail.com>
2018-05-25 14:36:53 +0300

    added PlaceContentProvider but not finish

--- app/src/main/java/com/example/stmak/learngoogleapi/provider/PlaceContentProvider.java
+++ app/src/main/java/com/example/stmak/learngoogleapi/provider/PlaceContentProvider.java
@@ -1,4 +1,121 @@
 package com.example.stmak.learngoogleapi.provider;
 
-public class PlaceContentProvider {
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class PlaceContentProvider extends ContentProvider {
+
+    public static final int PLACES = 100;
+    public static final int PLACE_WITH_ID = 101;
+
+    // Declare a static variable for the Uri matcher that you construct
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private static final String TAG = PlaceContentProvider.class.getName();
+
+    // Define a static buildUriMatcher methot that associates URI's with their int match
+    public static UriMatcher buildUriMatcher() {
+        // Initialize a UriMatcher
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        // Add URI matches
+        uriMatcher.addURI(PlaceContract.AUTHORITY, PlaceContract.PATH_PLACES, PLACES);
+        uriMatcher.addURI(PlaceContract.AUTHORITY, PlaceContract.PATH_PLACES + "/#", PLACE_WITH_ID);
+        return uriMatcher;
+    }
+
+    // Member variable for a PlaceDbHelper that's initialized in the onCreate() method
+    private PlaceDbHelper mPlaceDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mPlaceDbHelper = new PlaceDbHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        // Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mPlaceDbHelper.getReadableDatabase();
+
+        // Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        Cursor retCursor;
+
+        switch (match) {
+            case PLACES:
+                retCursor = db.query(PlaceContract.PlaceEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            // Default exception
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the desired Cursor
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        final SQLiteDatabase db = mPlaceDbHelper.getWritableDatabase();
+
+        // Write URI matching code to identify the match for the places directory
+        int match = sUriMatcher.match(uri);
+        Uri returnUri;
+        switch (match) {
+            case PLACES:
+                long id = db.insert(PlaceContract.PlaceEntry.TABLE_NAME, null, values);
+                if(id > 0) {
+                    returnUri = ContentUris.withAppendedId(PlaceContract.PlaceEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            // Default case throws an UnsupportedOperationException
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Notify the resolver if the uri has been changed, and return the newly inserted URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return constructed uri (this points to the newly inserted row of data)
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
 }
